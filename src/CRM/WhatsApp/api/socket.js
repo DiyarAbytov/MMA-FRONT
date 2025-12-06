d0fb60e2b1849699ebabad91cd6a6fe84997fab5
AbytovD <abytov247@gmail.com>
2025-12-07 03:07:59 +0600

    4
    4

--- src/CRM/WhatsApp/api/socket.js
+++
@@ -1,7 +0,0 @@
-// src/api/socket.js
-import { io } from "socket.io-client";
-import { config } from "../../../config/env";
-
-export const socket = io(config.WS_URL, {
-  transports: ["websocket"],
-});
