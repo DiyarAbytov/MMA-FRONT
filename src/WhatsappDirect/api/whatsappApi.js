344ebb7593149ac011e23b6061bab90d001ecc8a
AbytovD <abytov247@gmail.com>
2025-12-07 09:11:50 +0600

    6
    6

--- src/WhatsappDirect/api/whatsappApi.js
+++ src/WhatsappDirect/api/whatsappApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001", // на VPS заменишь на http://5.129.222.232:3001
+  baseURL: "http://5.129.222.232:3001", // на VPS заменишь на http://5.129.222.232:3001
 });
