dd4ab5cde09241639f2942e4df700234b01b0bae
AbytovD <abytov247@gmail.com>
2025-12-09 23:37:11 +0600

    2

--- src/WhatsappDirect/api/whatsappApi.js
+++ src/WhatsappDirect/api/whatsappApi.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:3001", // на VPS заменишь на http://5.129.222.232:3001
-    baseURL: "http://eva.adam247.webtm.ru/", // на VPS заменишь на http://5.129.222.232:3001
+    baseURL: "http://eva.adam247.webtm.ru", // на VPS заменишь на http://5.129.222.232:3001
 });
--- src/WhatsappDirect/components/utils/socket.js
+++ src/WhatsappDirect/components/utils/socket.js
@@ -2,5 +2,5 @@
 import { io } from "socket.io-client";
 
 // export const socket = io("http://localhost:3001"); // на VPS заменишь хост
-export const socket = io("http://eva.adam247.webtm.ru/"); // на VPS заменишь хост
+export const socket = io("http://eva.adam247.webtm.ru"); // на VPS заменишь хост
 // на VPS заменишь на "http://eva.adam247.webtm.ru/"
