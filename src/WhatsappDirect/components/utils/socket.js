53661870fb3db581f41369a0c6064cf550d8690d
AbytovD <abytov247@gmail.com>
2025-12-09 23:28:41 +0600

    new

--- src/WhatsappDirect/components/utils/socket.js
+++ src/WhatsappDirect/components/utils/socket.js
@@ -1,5 +1,6 @@
 // один общий socket для всего WhatsApp-модуля
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001"); // на VPS заменишь хост
-// на VPS заменишь на "http://5.129.222.232:3001"
+// export const socket = io("http://localhost:3001"); // на VPS заменишь хост
+export const socket = io("http://eva.adam247.webtm.ru/"); // на VPS заменишь хост
+// на VPS заменишь на "http://eva.adam247.webtm.ru/"
