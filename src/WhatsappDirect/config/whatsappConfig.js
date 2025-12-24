2b9b72c3b337266cc8002418be4418d6aecba10e
AbytovD <156185212+DiyarAbytov@users.noreply.github.com>
2025-12-25 04:01:10 +0600

    new

--- src/WhatsappDirect/config/whatsappConfig.js
+++ src/WhatsappDirect/config/whatsappConfig.js
@@ -1,3 +1,3 @@
 // whatsappConfig.js
-export const WHATSAPP_API_URL = "https://test2007.tw1.su/api";
+export const WHATSAPP_API_URL = "http://mashrapov.tw1.ru/api";
 // export const WHATSAPP_API_URL = "http://localhost:3001";
